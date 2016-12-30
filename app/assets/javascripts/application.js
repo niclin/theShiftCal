b75435c2ea34144812442b46a66be8b5f59ec00d
cndaniel <tigerljt@gmail.com>
2016-12-30 22:13:46 +0100

    navbar dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/alert
-//= require moment
-//= require bootstrap-datetimepicker
+//= require bootstrap/dropdown
 //= require_tree .
