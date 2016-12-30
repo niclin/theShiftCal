01cd2b3709eaaa74512aee96ae66838ac8e4e10b
cndaniel <tigerljt@gmail.com>
2016-12-30 18:32:18 +0100

    add seed and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-    User.create[email:"leonychuang@gmail.com", password:"yy2802"]
+  User.create([email:"leonychuang@gmail.com", password:"112802", password_confirmation:"112802", is_admin:"true"])
+
+  puts "created"
